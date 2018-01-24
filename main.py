32a38edc2c198c4e5b647efcd73fb398e787a871
Piotr Czajka <czajka@protonmail.com>
2018-01-24 20:56:34 +0100

    Get current time function, just say !godzina

--- main.py
+++ main.py
@@ -1,13 +1,47 @@
-from fbchat import Client
+import time
+
+from fbchat import Client, log
 from fbchat.models import Message, ThreadType
-import os
+
+from config import FBPASS, FBUSER, CHATNAME
 
 
-user = os.environ['FBUSER']
-password = os.environ['FBPASS']
+class WeatherBot(Client):
 
-client = Client(user, password)
+    def __init__(self, group_name, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.thread_id_ = self.searchForGroups(group_name)[0].uid
 
-users = client.fetchAllUsers()
+    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
+        if not thread_id == self.thread_id_:
+            log.info('Message is not sent in a group thread')
+            return
 
-print(users)
+        if not message_object.text.startswith('!'):
+            log.info('Message is not a command')
+            return
+
+        func = message_object.text[1:]
+
+        log.info(f'Invoking: {func}')
+
+        try:
+            func = getattr(self, func)
+        except AttributeError:
+            log.info(f'There is not such function as {func}')
+
+        func(self.thread_id_)
+
+    def godzina(self, thread_id):
+        curr_time = time.strftime('%H:%M:%S')
+        msg = Message(text=f'Jest godzina: {curr_time}')
+        self.send(msg, thread_id=thread_id, thread_type=ThreadType.GROUP)
+
+
+client = WeatherBot(CHATNAME, FBUSER, FBPASS)
+
+stillborn = client.searchForGroups(CHATNAME)[0]
+
+client.send(Message('Spytaj się mnie o godzinę'), thread_id=stillborn.uid, thread_type=ThreadType.GROUP)
+
+client.listen()
